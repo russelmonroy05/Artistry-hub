a90402d3358f5179929e581caaf2d573d7755f5b
russelmonroy05 <157871041+russelmonroy05@users.noreply.github.com>
2025-06-07 12:46:51 +0800

    Delete PROJECT directory

--- PROJECT/CORE/static/edit_profile.js
+++
@@ -1,8 +0,0 @@
-function previewImage(event) {
-    const reader = new FileReader();
-    reader.onload = function () {
-        const output = document.getElementById('profileImagePreview');
-        output.src = reader.result;
-    }
-    reader.readAsDataURL(event.target.files[0]);
-}
